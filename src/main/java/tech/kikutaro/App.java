9839fdd251e00f6bcfa0eba97f5ec901f2c353c2
kikutaro <kikutaro526@yahoo.co.jp>
2020-08-17 10:54:55 +0900

    ブログに合わせた修正とコメント整備

--- src/main/java/tech/kikutaro/App.java
+++ src/main/java/tech/kikutaro/App.java
@@ -4,18 +4,21 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
- * Hello world!
+ * Logbackのサンプルコード
  *
  */
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException
     {
+        //動作確認用
         System.out.println( "Hello World!" );
 
+        //ログ出力
         Logger logger = LoggerFactory.getLogger(App.class);
-        logger.error("hogehoge");
+        logger.error("エラーだよ");
 
-        Thread.sleep(30000);
+        //logback.xmlのasynchronousSendingがtrueの場合はスリープを入れる
+        //Thread.sleep(10000);
     }
 }
